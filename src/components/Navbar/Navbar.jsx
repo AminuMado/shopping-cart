224089bb5f42b52ec993a7672e08bfd209dca0e5
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-03 12:53:00 +0100

    added the shopping cart logo with items number

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function Navbar() {
   return (
     <div className="navbar-container">
-      <p>Retro Shop</p>
-      <p>Shop</p>
-      <p>About</p>
-      <p>shopping cart checkout icon</p>
+      <ul>
+        <li className="title">Retro Shop</li>
+        <li className="shop">Shop</li>
+        <li className="about">About</li>
+        <li className="shopping-cart-container">
+          <FontAwesomeIcon className="shopping-cart" icon={faCartShopping} />
+          <div className="shopping-cart-items-count">0</div>
+        </li>
+      </ul>
     </div>
   );
 }
