8fda09f6ed575c853de0f49fa5fdb946167a0eb1
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-03 13:11:34 +0100

    added class names to nav items

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 function Navbar() {
   return (
     <div className="navbar-container">
