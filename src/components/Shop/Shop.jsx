a7c8b203e840e1c7324051ace99064bf799a39dc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 20:30:50 +0100

    added a title container

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,8 @@
 import Card from "./Card/Card";
-
+import "./Shop.css";
 function Shop(props) {
   const { shoes } = props;
   const Shoes = shoes.map((shoe, index) => <Card shoe={shoe} key={index} />);
-  return <div>{Shoes}</div>;
+  return <div className="shop-container">{Shoes}</div>;
 }
 export default Shop;
