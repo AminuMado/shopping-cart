5d5ffaadfd3e8ba4cdde9867abcb7e90eb1c6d73
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 11:19:00 +0100

    made the card component dynamic and reusable

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
 import "./Card.css";
-import shoe_src from "../../Pollen_1.jpg";
+
 function Card(props) {
+  console.log(props);
+  const { Name, Src, Price, Favorite } = props.shoe;
   return (
     <div className="card-container">
-      <img src={shoe_src} alt="Jordans"></img>
+      <img src={Src} alt="Jordans"></img>
       <p className="card-name">Air Jordan 1s</p>
-      <p className="card-description">High Retro OG 'Pollen'</p>
-      <p className="card-price">$150</p>
-      <p className="card-recommended">Personal Favorite</p>
+      <p className="card-description">{Name}</p>
+      <p className="card-price">{Price}</p>
+      {Favorite && <p className="card-recommended">Personal Favorite</p>}
     </div>
   );
 }
