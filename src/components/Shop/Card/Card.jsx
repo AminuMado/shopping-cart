79ccde6d5d56ed46258b3e00c887c98f09b82e9b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 22:44:00 +0100

    passed the set active shoe props to card

--- src/components/Shop/Card/Card.jsx
+++ src/components/Shop/Card/Card.jsx
@@ -2,8 +2,9 @@ import "./Card.css";
 
 function Card(props) {
   const { Name, Src, Price, Favorite } = props.shoe;
+  const { handleClick } = props;
   return (
-    <div className="card-container">
+    <div className="card-container" onClick={(e) => handleClick(props.shoe)}>
       <img src={Src} alt="Jordans"></img>
       <p className="card-name">Air Jordan 1s</p>
       <p className="card-description">{Name}</p>
