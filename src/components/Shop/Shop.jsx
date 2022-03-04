e9c14516c701ffb48cc850d34e19bdd405d1af82
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 20:31:37 +0100

    remeoved redundancies

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,6 +3,11 @@ import "./Shop.css";
 function Shop(props) {
   const { shoes } = props;
   const Shoes = shoes.map((shoe, index) => <Card shoe={shoe} key={index} />);
-  return <div className="shop-container">{Shoes}</div>;
+  return (
+    <div className="shop-container">
+      <h1 className="title"> Jordan 1s Retro Collection</h1>
+      <div className="shoes-container">{Shoes}</div>
+    </div>
+  );
 }
 export default Shop;
