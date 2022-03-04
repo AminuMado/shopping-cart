566d206966c7192d30d91ae4aa841c2c760780e7
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 22:44:24 +0100

    passed the setActiveShoe props

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,16 @@
 import Card from "./Card/Card";
+import Details from "./Details/Details";
 import "./Shop.css";
 function Shop(props) {
-  const { shoes } = props;
-  const Shoes = shoes.map((shoe, index) => <Card shoe={shoe} key={index} />);
+  const { shoes, activeShoe, setActiveShoe } = props;
+  const Shoes = shoes.map((shoe, index) => (
+    <Card shoe={shoe} key={index} handleClick={setActiveShoe} />
+  ));
   return (
     <div className="shop-container">
+      {activeShoe && (
+        <Details activeShoe={activeShoe} handleClick={setActiveShoe} />
+      )}
       <h1 className="title"> Jordan 1s Retro Collection</h1>
       <div className="shoes-container">{Shoes}</div>
     </div>
