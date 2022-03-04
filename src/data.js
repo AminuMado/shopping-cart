19233544bd1d5b544a2c1fb646fa42dae2a523fa
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 10:55:59 +0100

    added the shoes as imports and data to mimic a fetch call

---
+++ src/data.js
@@ -0,0 +1,380 @@
+import A_Star_is_Born_1_Src from "./Assets/Images/A_Star_is_Born_1.jpg";
+import A_Star_is_Born_2_Src from "./Assets/Images/A_Star_is_Born_2.jpg";
+import A_Star_is_Born_3_Src from "./Assets/Images/A_Star_is_Born_3.jpg";
+import A_Star_is_Born_4_Src from "./Assets/Images/A_Star_is_Born_4.jpg";
+import Barons_1_Src from "./Assets/Images/Barons_1.jpg";
+import Barons_2_Src from "./Assets/Images/Barons_2.jpg";
+import Barons_3_Src from "./Assets/Images/Barons_3.jpg";
+import Barons_4_Src from "./Assets/Images/Barons_4.jpg";
+import Black_Gym_Red_1_Src from "./Assets/Images/Black_Gym_Red_1.jpg";
+import Black_Gym_Red_2_Src from "./Assets/Images/Black_Gym_Red_2.jpg";
+import Black_Gym_Red_3_Src from "./Assets/Images/Black_Gym_Red_3.jpg";
+import Black_Gym_Red_4_Src from "./Assets/Images/Black_Gym_Red_4.jpg";
+import Bloodline_1_Src from "./Assets/Images/Bloodline_1.jpg";
+import Bloodline_2_Src from "./Assets/Images/Bloodline_2.jpg";
+import Bloodline_3_Src from "./Assets/Images/Bloodline_3.jpg";
+import Bloodline_4_Src from "./Assets/Images/Bloodline_4.jpg";
+import Bred_1_Src from "./Assets/Images/Bred_1.jpg";
+import Bred_2_Src from "./Assets/Images/Bred_2.jpg";
+import Bred_3_Src from "./Assets/Images/Bred_3.jpg";
+import Bred_4_Src from "./Assets/Images/Bred_4.jpg";
+import City_of_Flight_1_Src from "./Assets/Images/City_of_Flight_1.jpg";
+import City_of_Flight_2_Src from "./Assets/Images/City_of_Flight_2.jpg";
+import City_of_Flight_3_Src from "./Assets/Images/City_of_Flight_3.jpg";
+import City_of_Flight_4_Src from "./Assets/Images/City_of_Flight_4.jpg";
+import Clay_Green_1_Src from "./Assets/Images/Clay_Green_1.jpg";
+import Clay_Green_2_Src from "./Assets/Images/Clay_Green_2.jpg";
+import Clay_Green_3_Src from "./Assets/Images/Clay_Green_3.jpg";
+import Clay_Green_4_Src from "./Assets/Images/Clay_Green_4.jpg";
+import Court_Purple_1_Src from "./Assets/Images/Court_Purple_1.jpg";
+import Court_Purple_2_Src from "./Assets/Images/Court_Purple_2.jpg";
+import Court_Purple_3_Src from "./Assets/Images/Court_Purple_3.jpg";
+import Court_Purple_4_Src from "./Assets/Images/Court_Purple_4.jpg";
+import Dark_Marina_Blue_1_Src from "./Assets/Images/Dark_Marina_Blue_1.jpg";
+import Dark_Marina_Blue_2_Src from "./Assets/Images/Dark_Marina_Blue_2.jpg";
+import Dark_Marina_Blue_3_Src from "./Assets/Images/Dark_Marina_Blue_3.jpg";
+import Dark_Marina_Blue_4_Src from "./Assets/Images/Dark_Marina_Blue_4.jpg";
+import Gym_Red_1_Src from "./Assets/Images/Gym_Red_1.jpg";
+import Gym_Red_2_Src from "./Assets/Images/Gym_Red_2.jpg";
+import Gym_Red_3_Src from "./Assets/Images/Gym_Red_3.jpg";
+import Gym_Red_4_Src from "./Assets/Images/Gym_Red_4.jpg";
+import Hyper_Royal_1_Src from "./Assets/Images/Hyper_Royal_1.jpg";
+import Hyper_Royal_2_Src from "./Assets/Images/Hyper_Royal_2.jpg";
+import Hyper_Royal_3_Src from "./Assets/Images/Hyper_Royal_3.jpg";
+import Hyper_Royal_4_Src from "./Assets/Images/Hyper_Royal_4.jpg";
+import NYC_to_Paris_1_Src from "./Assets/Images/NYC_to_Paris_1.jpg";
+import NYC_to_Paris_2_Src from "./Assets/Images/NYC_to_Paris_2.jpg";
+import NYC_to_Paris_3_Src from "./Assets/Images/NYC_to_Paris_3.jpg";
+import NYC_to_Paris_4_Src from "./Assets/Images/NYC_to_Paris_4.jpg";
+import Obsidian_1_Src from "./Assets/Images/Obsidian_1.jpg";
+import Obsidian_2_Src from "./Assets/Images/Obsidian_2.jpg";
+import Obsidian_3_Src from "./Assets/Images/Obsidian_3.jpg";
+import Obsidian_4_Src from "./Assets/Images/Obsidian_4.jpg";
+import Pine_Green_1_Src from "./Assets/Images/Pine_Green_1.jpg";
+import Pine_Green_2_Src from "./Assets/Images/Pine_Green_2.jpg";
+import Pine_Green_3_Src from "./Assets/Images/Pine_Green_3.jpg";
+import Pine_Green_4_Src from "./Assets/Images/Pine_Green_4.jpg";
+import Pollen_1_Src from "./Assets/Images/Pollen_1.jpg";
+import Pollen_2_Src from "./Assets/Images/Pollen_2.jpg";
+import Pollen_3_Src from "./Assets/Images/Pollen_3.jpg";
+import Pollen_4_Src from "./Assets/Images/Pollen_4.jpg";
+import RE2PECT_1_Src from "./Assets/Images/RE2PECT_1.jpg";
+import RE2PECT_2_Src from "./Assets/Images/RE2PECT_2.jpg";
+import RE2PECT_3_Src from "./Assets/Images/RE2PECT_3.jpg";
+import RE2PECT_4_Src from "./Assets/Images/RE2PECT_4.jpg";
+import Royal_Toe_1_Src from "./Assets/Images/Royal_Toe_1.jpg";
+import Royal_Toe_2_Src from "./Assets/Images/Royal_Toe_2.jpg";
+import Royal_Toe_3_Src from "./Assets/Images/Royal_Toe_3.jpg";
+import Royal_Toe_4_Src from "./Assets/Images/Royal_Toe_4.jpg";
+import Shadow_1_Src from "./Assets/Images/Shadow_1.jpg";
+import Shadow_2_Src from "./Assets/Images/Shadow_2.jpg";
+import Shadow_3_Src from "./Assets/Images/Shadow_3.jpg";
+import Shadow_4_Src from "./Assets/Images/Shadow_4.jpg";
+import Tokyo_Bio_Hack_1_Src from "./Assets/Images/Tokyo_Bio_Hack_1.jpg";
+import Tokyo_Bio_Hack_2_Src from "./Assets/Images/Tokyo_Bio_Hack_2.jpg";
+import Tokyo_Bio_Hack_3_Src from "./Assets/Images/Tokyo_Bio_Hack_3.jpg";
+import Tokyo_Bio_Hack_4_Src from "./Assets/Images/Tokyo_Bio_Hack_4.jpg";
+import Top_3_1_Src from "./Assets/Images/Top_3_1.jpg";
+import Top_3_2_Src from "./Assets/Images/Top_3_2.jpg";
+import Top_3_3_Src from "./Assets/Images/Top_3_3.jpg";
+import Top_3_4_Src from "./Assets/Images/Top_3_4.jpg";
+import Volt_Gold_1_Src from "./Assets/Images/Volt_Gold_1.jpg";
+import Volt_Gold_2_Src from "./Assets/Images/Volt_Gold_2.jpg";
+import Volt_Gold_3_Src from "./Assets/Images/Volt_Gold_3.jpg";
+import Volt_Gold_4_Src from "./Assets/Images/Volt_Gold_4.jpg";
+import White_Elephant_1_Src from "./Assets/Images/White_Elephant_1.jpg";
+import White_Elephant_2_Src from "./Assets/Images/White_Elephant_2.jpg";
+import White_Elephant_3_Src from "./Assets/Images/White_Elephant_3.jpg";
+import White_Elephant_4_Src from "./Assets/Images/White_Elephant_4.jpg";
+import White_Shadow_1_Src from "./Assets/Images/White_Shadow_1.jpg";
+import White_Shadow_2_Src from "./Assets/Images/White_Shadow_2.jpg";
+import White_Shadow_3_Src from "./Assets/Images/White_Shadow_3.jpg";
+import White_Shadow_4_Src from "./Assets/Images/White_Shadow_4.jpg";
+import World_Championship_1_Src from "./Assets/Images/World_Championship_1.jpg";
+import World_Championship_2_Src from "./Assets/Images/World_Championship_2.jpg";
+import World_Championship_3_Src from "./Assets/Images/World_Championship_3.jpg";
+import World_Championship_4_Src from "./Assets/Images/World_Championship_4.jpg";
+import Yin_Yang_1_Src from "./Assets/Images/Yin_Yang_1.jpg";
+import Yin_Yang_2_Src from "./Assets/Images/Yin_Yang_2.jpg";
+import Yin_Yang_3_Src from "./Assets/Images/Yin_Yang_3.jpg";
+import Yin_Yang_4_Src from "./Assets/Images/Yin_Yang_4.jpg";
+import Yin_Yang_1b_Src from "./Assets/Images/Yin_Yang_1b.jpg";
+import Yin_Yang_2b_Src from "./Assets/Images/Yin_Yang_2b.jpg";
+import Yin_Yang_3b_Src from "./Assets/Images/Yin_Yang_3b.jpg";
+import Yin_Yang_4b_Src from "./Assets/Images/Yin_Yang_4b.jpg";
+import Zen_Green_1_Src from "./Assets/Images/Zen_Green_1.jpg";
+import Zen_Green_2_Src from "./Assets/Images/Zen_Green_2.jpg";
+import Zen_Green_3_Src from "./Assets/Images/Zen_Green_3.jpg";
+import Zen_Green_4_Src from "./Assets/Images/Zen_Green_4.jpg";
+
+const Shoe = [
+  {
+    Name: `Retro High OG 'A Star is Born'`,
+    Src: A_Star_is_Born_1_Src,
+    Photos: [
+      A_Star_is_Born_1_Src,
+      A_Star_is_Born_2_Src,
+      A_Star_is_Born_3_Src,
+      A_Star_is_Born_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Barons'`,
+    Src: Barons_1_Src,
+    Photos: [Barons_1_Src, Barons_2_Src, Barons_3_Src, Barons_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Black_Gym_Red'`,
+    Src: Black_Gym_Red_1_Src,
+    Photos: [
+      Black_Gym_Red_1_Src,
+      Black_Gym_Red_2_Src,
+      Black_Gym_Red_3_Src,
+      Black_Gym_Red_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Bloodline'`,
+    Src: Bloodline_1_Src,
+    Photos: [
+      Bloodline_1_Src,
+      Bloodline_2_Src,
+      Bloodline_3_Src,
+      Bloodline_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Bred'`,
+    Src: Bred_1_Src,
+    Photos: [Bred_1_Src, Bred_2_Src, Bred_3_Src, Bred_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'City of Flight'`,
+    Src: City_of_Flight_1_Src,
+    Photos: [
+      City_of_Flight_1_Src,
+      City_of_Flight_2_Src,
+      City_of_Flight_3_Src,
+      City_of_Flight_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Clay Green'`,
+    Src: Clay_Green_1_Src,
+    Photos: [
+      Clay_Green_1_Src,
+      Clay_Green_2_Src,
+      Clay_Green_3_Src,
+      Clay_Green_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Court Purple'`,
+    Src: Court_Purple_1_Src,
+    Photos: [
+      Court_Purple_1_Src,
+      Court_Purple_2_Src,
+      Court_Purple_3_Src,
+      Court_Purple_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Dark Marina Blue'`,
+    Src: Dark_Marina_Blue_1_Src,
+    Photos: [
+      Dark_Marina_Blue_1_Src,
+      Dark_Marina_Blue_2_Src,
+      Dark_Marina_Blue_3_Src,
+      Dark_Marina_Blue_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Gym Red'`,
+    Src: Gym_Red_1_Src,
+    Photos: [Gym_Red_1_Src, Gym_Red_2_Src, Gym_Red_3_Src, Gym_Red_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Hyper Royal'`,
+    Src: Hyper_Royal_1_Src,
+    Photos: [
+      Hyper_Royal_1_Src,
+      Hyper_Royal_2_Src,
+      Hyper_Royal_3_Src,
+      Hyper_Royal_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'NYC to Paris'`,
+    Src: NYC_to_Paris_1_Src,
+    Photos: [
+      NYC_to_Paris_1_Src,
+      NYC_to_Paris_2_Src,
+      NYC_to_Paris_3_Src,
+      NYC_to_Paris_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Obsidian'`,
+    Src: Obsidian_1_Src,
+    Photos: [Obsidian_1_Src, Obsidian_2_Src, Obsidian_3_Src, Obsidian_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Pine Green 2.0'`,
+    Src: Pine_Green_1_Src,
+    Photos: [
+      Pine_Green_1_Src,
+      Pine_Green_2_Src,
+      Pine_Green_3_Src,
+      Pine_Green_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Pollen'`,
+    Src: Pollen_1_Src,
+    Photos: [Pollen_1_Src, Pollen_2_Src, Pollen_3_Src, Pollen_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'RE2PECT'`,
+    Src: RE2PECT_1_Src,
+    Photos: [RE2PECT_1_Src, RE2PECT_2_Src, RE2PECT_3_Src, RE2PECT_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Royal Toe'`,
+    Src: Royal_Toe_1_Src,
+    Photos: [
+      Royal_Toe_1_Src,
+      Royal_Toe_2_Src,
+      Royal_Toe_3_Src,
+      Royal_Toe_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Shadow'`,
+    Src: Shadow_1_Src,
+    Photos: [Shadow_1_Src, Shadow_2_Src, Shadow_3_Src, Shadow_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Tokyo Bio Hack'`,
+    Src: Tokyo_Bio_Hack_1_Src,
+    Photos: [
+      Tokyo_Bio_Hack_1_Src,
+      Tokyo_Bio_Hack_2_Src,
+      Tokyo_Bio_Hack_3_Src,
+      Tokyo_Bio_Hack_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Top 3'`,
+    Src: Top_3_1_Src,
+    Photos: [Top_3_1_Src, Top_3_2_Src, Top_3_3_Src, Top_3_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'Volt Gold'`,
+    Src: Volt_Gold_1_Src,
+    Photos: [
+      Volt_Gold_1_Src,
+      Volt_Gold_2_Src,
+      Volt_Gold_3_Src,
+      Volt_Gold_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'White Elephant'`,
+    Src: White_Elephant_1_Src,
+    Photos: [
+      White_Elephant_1_Src,
+      White_Elephant_2_Src,
+      White_Elephant_3_Src,
+      White_Elephant_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'White Shadow'`,
+    Src: White_Shadow_1_Src,
+    Photos: [
+      White_Shadow_1_Src,
+      White_Shadow_2_Src,
+      White_Shadow_3_Src,
+      White_Shadow_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `High Retro OG 'World Championship'`,
+    Src: World_Championship_1_Src,
+    Photos: [
+      World_Championship_1_Src,
+      World_Championship_2_Src,
+      World_Championship_3_Src,
+      World_Championship_4_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `Retro High OG 'Yin Yang White'`,
+    Src: Yin_Yang_1_Src,
+    Photos: [Yin_Yang_1_Src, Yin_Yang_2_Src, Yin_Yang_3_Src, Yin_Yang_4_Src],
+    Price: `$150`,
+    Favorite: false,
+  },
+  {
+    Name: `Retro High OG 'Yin Yang Black'`,
+    Src: Yin_Yang_1b_Src,
+    Photos: [
+      Yin_Yang_1b_Src,
+      Yin_Yang_2b_Src,
+      Yin_Yang_3b_Src,
+      Yin_Yang_4b_Src,
+    ],
+    Price: `$150`,
+    Favorite: false,
+  },
+];
+
+export default Shoe;
