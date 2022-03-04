59dc5493e23cf64782c87b44221827ff31f3aa08
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 11:19:22 +0100

    added the shoe and data files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
 import "./App.css";
+import Card from "./components/Card/Card";
 import Landing from "./components/Landing/Landing";
 import Navbar from "./components/Navbar/Navbar";
+import data from "./data";
 
 function App() {
+  const [shoes, setShoes] = React.useState(data);
+  const Shoe = shoes.map((shoe, index) => <Card shoe={shoe} key={index} />);
   return (
     <div>
       {/* <Landing /> */}
       <Navbar></Navbar>
+      {Shoe}
     </div>
   );
 }
