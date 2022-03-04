28d4a7e1ce66e3386d3455fa2261e39727883bc5
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-04 22:45:10 +0100

    linked with css

---
+++ src/components/Shop/Details/Details.jsx
@@ -0,0 +1,35 @@
+import "./Details.css";
+
+function Details(props) {
+  const { handleClick, activeShoe } = props;
+
+  const generatePhotos = (photos) => {
+    return photos.map((photo) => <img src={photo} alt="Jordans"></img>);
+  };
+  const photos = generatePhotos(activeShoe.Photos);
+  return (
+    // <div className="details-overlay active">
+    //   <div className="details active">
+
+    //     <div className="slides">{photos}</div>
+    //     <p className="card-name">Air Jordan 1s</p>
+    //     <p className="card-description"></p>
+    //     <p className="card-price"></p>
+    //     <button onClick={(e) => handleClick(false)}>Click Me</button>
+    //   </div>
+    // </div>
+    <form>
+      <input type="radio" name="fancy" autofocus value="clubs" id="clubs" />
+      <input type="radio" name="fancy" value="hearts" id="hearts" />
+      <input type="radio" name="fancy" value="spades" id="spades" />
+      <input type="radio" name="fancy" value="diamonds" id="diamonds" />
+      <label for="clubs">&#9827; Clubs</label>
+      <label for="hearts">&#9829; Hearts</label>
+      <label for="spades">&#9824; Spades</label>
+      <label for="diamonds">&#9830; Diamonds</label>
+
+      <div class="keys">Use left and right keys to navigate</div>
+    </form>
+  );
+}
+export default Details;
