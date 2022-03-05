4523d2f8f9d000d19e30a8d90a90bbf02be76367
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-05 22:11:57 +0100

    created handleaddtocart function

--- src/components/Shop/Details/Details.jsx
+++ src/components/Shop/Details/Details.jsx
@@ -4,12 +4,18 @@ import Carousel from "../../Carousel/Carousel";
 import { CarouselItem } from "../../Carousel/Carousel";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 function Details(props) {
-  const { handleClick, activeShoe } = props;
+  const { handleClick, activeShoe, setCartItems } = props;
   const photos = activeShoe.Photos.map((photo, index) => (
     <CarouselItem key={index}>
       <img src={photo} alt="Jordans"></img>
     </CarouselItem>
   ));
+  function handleAddToCart(shoe) {
+    //if shoe exists in the cart
+    // increase its quantity
+    // if it doesnt just add it
+    // so you need to know what cart is
+  }
   return (
     <div className="details-overlay active">
       <div className="details active">
