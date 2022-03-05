e8d08569cb6ee39ab3b2f125f33cbbd564ee9015
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-05 22:11:34 +0100

    passed down cart state variable

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,14 +2,18 @@ import Card from "./Card/Card";
 import Details from "./Details/Details";
 import "./Shop.css";
 function Shop(props) {
-  const { shoes, activeShoe, setActiveShoe } = props;
+  const { shoes, activeShoe, setActiveShoe, setCartItems } = props;
   const Shoes = shoes.map((shoe, index) => (
     <Card shoe={shoe} key={index} handleClick={setActiveShoe} />
   ));
   return (
     <div className="shop-container">
       {activeShoe && (
-        <Details activeShoe={activeShoe} handleClick={setActiveShoe} />
+        <Details
+          activeShoe={activeShoe}
+          handleClick={setActiveShoe}
+          setCartItems={setCartItems}
+        />
       )}
       <h1 className="title"> Jordan 1s Retro Collection</h1>
       <div className="shoes-container">{Shoes}</div>
