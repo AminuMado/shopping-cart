a90e4b2824fdf00debf943082cd1b41101a8ea6f
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-05 18:13:29 +0100

    added back the photos for rendering

--- src/components/Shop/Details/Details.jsx
+++ src/components/Shop/Details/Details.jsx
@@ -4,33 +4,23 @@ import { CarouselItem } from "../../Carousel/Carousel";
 function Details(props) {
   const { handleClick, activeShoe } = props;
 
-  const generatePhotos = (photos) => {
-    return photos.map((photo) => <img src={photo} alt="Jordans"></img>);
-  };
-  const photos = generatePhotos(activeShoe.Photos);
+  const photos = activeShoe.Photos.map((photo) => (
+    <CarouselItem>
+      <img src={photo} alt="Jordans"></img>
+    </CarouselItem>
+  ));
   return (
     <div className="details-overlay active">
       <div className="details active">
-        <div className="slides">{photos}</div>
-        <p className="card-name">Air Jordan 1s</p>
-        <p className="card-description"></p>
-        <p className="card-price"></p>
-        <button onClick={(e) => handleClick(false)}>Click Me</button>
+        <Carousel>{photos}</Carousel>
+        <div>
+          <p className="card-name">Air Jordan 1s</p>
+          <p className="card-description">{activeShoe.Name}</p>
+          <p className="card-price">{activeShoe.Price}</p>
+          <button onClick={(e) => handleClick(false)}>Click Me</button>
+        </div>
       </div>
     </div>
   );
 }
 export default Details;
-
-// <form>
-//   <input type="radio" name="fancy" autofocus value="clubs" id="clubs" />
-//   <input type="radio" name="fancy" value="hearts" id="hearts" />
-//   <input type="radio" name="fancy" value="spades" id="spades" />
-//   <input type="radio" name="fancy" value="diamonds" id="diamonds" />
-//   <label for="clubs">&#9827; Clubs</label>
-//   <label for="hearts">&#9829; Hearts</label>
-//   <label for="spades">&#9824; Spades</label>
-//   <label for="diamonds">&#9830; Diamonds</label>
-
-//   <div class="keys">Use left and right keys to navigate</div>
-// </form>
