4a5d3198ca324bec168ba287d77aaff790e3140c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-05 17:48:28 +0100

    updated the comments

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -6,6 +6,15 @@ import {
   faArrowRightLong,
 } from "@fortawesome/free-solid-svg-icons";
 
+//This is function that takes items and displays them
+// it lines them up in a carousel
+// in the code below React.Children.map
+//The code below shows the difference
+//  var photo = null
+// photo.map(i => {return i}) // => VM370:1 Uncaught TypeError: Cannot read property 'map' of null
+// React.Children.map(photo, i => {return i;}) // return null
+// note props.children returns anything you pass inbetwwen  the opening and closing tags of the comoponent <Carousel> these constitiue the children </Carousel>
+// this compoenent is dependednt on the children you call between the CarouselItem tags this could be anything
 export function CarouselItem(props) {
   return (
     <div className="carousel-item" style={{ width: props.width }}>
@@ -25,17 +34,6 @@ function Carousel(props) {
     } else newIndex = currentIndex;
     setActiveIndex(newIndex);
   }
-
-  //This is function that takes photos and displays them
-  //so it should depend on props which youll pass a list of photos and
-  // it lines them up in a carousel
-  // in the code below React.Children.map is used while you could also use
-  // props.photos.map and acheive the same result but using the React API
-  //The code below shows the difference
-  //  var photo = null
-  // photo.map(i => {return i}) // => VM370:1 Uncaught TypeError: Cannot read property 'map' of null
-  // React.Children.map(photo, i => {return i;}) // return null
-  // note props.children returns anything you pass inbetwwen  the opening and closing tags of the comoponent <Carousel> these constitiue the children </Carousel>
 
   return (
     <div className="carousel">
