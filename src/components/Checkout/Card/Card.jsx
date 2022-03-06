b176f146dbee82e3b0b71680ab323506930e644b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-06 22:55:26 +0100

    added delete funtion

--- src/components/Checkout/Card/Card.jsx
+++ src/components/Checkout/Card/Card.jsx
@@ -2,15 +2,16 @@ import "./Card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
-import photo from "../../../Assets/Images/Zen_Green_1.jpg";
 function Card(props) {
+  const { Name, Src, Price, Quantity, Id } = props.shoe;
+  function deleteItem() {}
   return (
     <div className="checkout-card-container">
-      <img src={photo} alt="Jordans"></img>
+      <img src={Src} alt="Jordans"></img>
       <div className="checkout-card-details">
-        <h3 className="checkout-card-name">Zen Green</h3>
-        <p className="checkout-card-quantity">{`Quantity: 2`}</p>
-        <p className="checkout-card-price">Price: $250 VAT included</p>
+        <h3 className="checkout-card-name">{Name}</h3>
+        <p className="checkout-card-quantity">{`Quantity: ${Quantity}`}</p>
+        <p className="checkout-card-price">{`Price: ${Price} VAT included`}</p>
       </div>
       <FontAwesomeIcon className="trash-icon" icon={faTrashCan} />
     </div>
