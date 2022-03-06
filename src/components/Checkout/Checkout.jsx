d9c48aa089d1e2826379386558125d66853677ea
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-06 22:55:02 +0100

    added the cartitem and setcartitem state variables to card component

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -2,10 +2,19 @@ import "./Checkout.css";
 import Card from "./Card/Card";
 import Summary from "./Summary/Summary";
 function Checkout(props) {
+  const { cartItems, setCartItems } = props;
+  const shoes = cartItems.map((shoe, index) => (
+    <Card
+      shoe={shoe}
+      key={index}
+      cartItems={cartItems}
+      setCartItems={setCartItems}
+    />
+  ));
   return (
     <div className="checkout-container">
       <h1>My Cart</h1>
-      <Card />
+      <div>{shoes}</div>
       <Summary />
     </div>
   );
