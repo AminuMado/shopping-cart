0ccfb0bee637726140c12dd7f6eaf2618dd65cfc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-06 21:37:32 +0100

    added price and quantity templates

--- src/components/Checkout/Card/Card.jsx
+++ src/components/Checkout/Card/Card.jsx
@@ -1,5 +1,16 @@
 import "./Card.css";
+import photo from "../../../Assets/Images/Zen_Green_1.jpg";
 function Card(props) {
-  return <h1>Hello Card</h1>;
+  return (
+    <div className="checkout-card-container">
+      <img src={photo} alt="Jordans"></img>
+      <div className="checkout-card-details">
+        <p className="checkout-card-name">Air Jordan 1s</p>
+        <p className="checkout-card-description">Zen Green</p>
+        <p className="checkout-card-quantity">2</p>
+        <p className="checkout-card-price">$250</p>
+      </div>
+    </div>
+  );
 }
 export default Card;
