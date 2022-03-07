4c4208c0d24a6b48298ddfe9bc5fe4a8c6a0c321
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-07 12:55:42 +0100

    added the elements

--- src/components/Checkout/Summary/Summary.jsx
+++ src/components/Checkout/Summary/Summary.jsx
@@ -1,5 +1,34 @@
 import "./Summary.css";
 function Summary(props) {
-  return <h1>Hello Summary</h1>;
+  return (
+    <div class="cart-page-summary-container">
+      <h1>Order summary</h1>
+      <span class="cart-page-item-subinfo-detail">0 items</span>
+      <div class="cart-page-summary-info">
+        <span class="cart-page-summary-info-left">
+          <strong>Subtotal:</strong>
+        </span>
+        <span class="cart-page-summary-info-right">$0.00</span>
+      </div>
+      <div class="cart-page-summary-info">
+        <span class="cart-page-summary-info-left">
+          <strong>Shipping:</strong>
+        </span>
+        <span class="cart-page-summary-info-right">$0.00</span>
+      </div>
+      <span class="free-shipping" id="free-shipping">
+        (free shipping on orders over $150!)
+      </span>
+      <div class="cart-page-summary-info overline">
+        <span class="cart-page-summary-info-left">
+          <strong>Total:</strong>
+        </span>
+        <span class="cart-page-summary-info-right">
+          <strong>$0.00</strong>
+        </span>
+      </div>
+      <button class="checkout-btn">checkout</button>
+    </div>
+  );
 }
 export default Summary;
