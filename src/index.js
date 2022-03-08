1e67f3216a1b107388b64e1f231bdb5f1bb03587
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-08 21:39:56 +0100

    imported routeswitch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import RouteSwitch from "./RouteSwitch";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouteSwitch />
   </React.StrictMode>,
   document.getElementById("root")
 );
