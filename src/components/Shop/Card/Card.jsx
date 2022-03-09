17497cf718b33ac79071bc95945d0cbc33d97365
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-09 20:24:31 +0100

    added float to price

--- src/components/Shop/Card/Card.jsx
+++ src/components/Shop/Card/Card.jsx
@@ -7,7 +7,7 @@ function Card({ Name, Src, Price, Favorite, id }) {
         <img src={Src} alt="Jordans"></img>
         <p className="card-name">Air Jordan 1s</p>
         <p className="card-description">{Name}</p>
-        <p className="card-price">${Price}</p>
+        <p className="card-price">${Price.toFixed(2)}</p>
         {Favorite && <p className="card-recommended">Personal Favorite</p>}
       </div>
     </Link>
