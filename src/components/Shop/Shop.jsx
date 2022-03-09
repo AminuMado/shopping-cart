708b97a01106a93cbdb8dc273cfdcdff48194190
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-09 20:23:03 +0100

    cleaned up the code

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,7 @@
 import Card from "./Card/Card";
 import "./Shop.css";
 function Shop({ items }) {
-  const Shoes = items.map((shoe, index) => <Card shoe={shoe} key={index} />);
+  const Shoes = items.map((shoe, index) => <Card {...shoe} key={index} />);
   return (
     <div className="shop-container">
       <h1 className="title"> Jordan 1s Retro Collection</h1>
