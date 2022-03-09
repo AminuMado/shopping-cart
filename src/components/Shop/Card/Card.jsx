c463575e5f669c958bd696555c1c472e8c1e2ae2
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-09 20:20:30 +0100

    added link to the component

--- src/components/Shop/Card/Card.jsx
+++ src/components/Shop/Card/Card.jsx
@@ -1,16 +1,18 @@
 import "./Card.css";
-
+import { Link } from "react-router-dom";
 function Card(props) {
-  const { Name, Src, Price, Favorite } = props.shoe;
-  const { handleClick } = props;
+  const { Name, Src, Price, Favorite, id } = props.shoe;
+
   return (
-    <div className="card-container" onClick={(e) => handleClick(props.shoe)}>
-      <img src={Src} alt="Jordans"></img>
-      <p className="card-name">Air Jordan 1s</p>
-      <p className="card-description">{Name}</p>
-      <p className="card-price">${Price}</p>
-      {Favorite && <p className="card-recommended">Personal Favorite</p>}
-    </div>
+    <Link to={`/shop/${id}`}>
+      <div className="card-container">
+        <img src={Src} alt="Jordans"></img>
+        <p className="card-name">Air Jordan 1s</p>
+        <p className="card-description">{Name}</p>
+        <p className="card-price">${Price}</p>
+        {Favorite && <p className="card-recommended">Personal Favorite</p>}
+      </div>
+    </Link>
   );
 }
 export default Card;
