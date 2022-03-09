0fbf0fe2e4466d1fb79bc78be0daba118eabb3fa
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-09 20:20:10 +0100

    changed code format to include routing

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,21 +1,9 @@
 import Card from "./Card/Card";
-import Details from "./Details/Details";
 import "./Shop.css";
-function Shop(props) {
-  const { shoes, activeShoe, setActiveShoe, cartItems, setCartItems } = props;
-  const Shoes = shoes.map((shoe, index) => (
-    <Card shoe={shoe} key={index} handleClick={setActiveShoe} />
-  ));
+function Shop({ items }) {
+  const Shoes = items.map((shoe, index) => <Card shoe={shoe} key={index} />);
   return (
     <div className="shop-container">
-      {activeShoe && (
-        <Details
-          activeShoe={activeShoe}
-          setActiveShoe={setActiveShoe}
-          cartItems={cartItems}
-          setCartItems={setCartItems}
-        />
-      )}
       <h1 className="title"> Jordan 1s Retro Collection</h1>
       <div className="shoes-container">{Shoes}</div>
     </div>
