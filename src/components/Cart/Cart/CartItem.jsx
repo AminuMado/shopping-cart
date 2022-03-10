b1f2041c13b1744125392fee220d3882dfa0143b
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-10 09:02:09 +0100

    refactored to use routing

--- src/components/Cart/Cart/CartItem.jsx
+++ src/components/Cart/Cart/CartItem.jsx
@@ -1,28 +1,45 @@
-import "./Card.css";
+import "./CartItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-function Card(props) {
-  const { cartItems, setCartItems, shoe } = props;
-  const { Name, Src, Price, Quantity } = shoe;
-  function deleteItem(shoe) {
-    const newCartItems = cartItems.filter((item) => item.id !== shoe.id);
-    setCartItems(newCartItems);
-  }
+function CartItem({
+  id,
+  Name,
+  Src,
+  Price,
+  Quantity,
+  deleteCartItem,
+  changeQuantity,
+}) {
+  const handleDelete = () => deleteCartItem(id);
+  const handleIncrease = () => changeQuantity(id, 1);
+  const handleDecrease = () => changeQuantity(id, -1);
   return (
-    <div className="checkout-card-container">
+    <div className="cart-item-container">
       <img src={Src} alt="Jordans"></img>
-      <div className="checkout-card-details">
-        <h3 className="checkout-card-name">{Name}</h3>
-        <p className="checkout-card-quantity">{`Quantity: ${Quantity}`}</p>
-        <p className="checkout-card-price">{`Price: ${Price} VAT included`}</p>
+      <div className="cart-item-details">
+        <Link to={`/shop/${id}`}>
+          <h3 className="cart-item-name">{Name}</h3>{" "}
+        </Link>
+        <p className="cart-item-quantity">{`Quantity: ${Quantity}`}</p>
+        <p className="cart-item-price">{`Price: ${Price.toFixed(
+          2
+        )} VAT included`}</p>
       </div>
       <FontAwesomeIcon
         className="trash-icon"
         icon={faTrashCan}
-        onClick={() => deleteItem(shoe)}
+        onClick={() => handleDelete(id)}
       />
+      <div className="quantity-btns-container">
+        <button onClick={handleDecrease} disabled={Quantity < 2}>
+          -
+        </button>
+        <span>{Quantity}</span>
+        <button onClick={handleIncrease}>+</button>
+      </div>
     </div>
   );
 }
-export default Card;
+export default CartItem;
