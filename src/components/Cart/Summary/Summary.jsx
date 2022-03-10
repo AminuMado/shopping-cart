649fac19753ada4deb9839f4b93818b35dbbacdc
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-10 23:08:06 +0100

    updated code

--- src/components/Cart/Summary/Summary.jsx
+++ src/components/Cart/Summary/Summary.jsx
@@ -1,62 +1,20 @@
 import "./Summary.css";
-function Summary(props) {
-  const { cartItems } = props;
+import { faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
-  const cartSubtotalPrice = cartItems
+function Summary({ cartItems }) {
+  const cartTotalPrice = cartItems
     .map((item) => item.Price * item.Quantity)
     .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
     .toFixed(2);
 
-  const cartTotalItems = cartItems
-    .map((item) => parseInt(item.Quantity))
-    .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-
-  let shipping = (0.0).toFixed(2);
-  let freeShipping = `$${
-    500 - parseFloat(cartSubtotalPrice)
-  } away from free shipping!`;
-
-  if (cartSubtotalPrice > 499.99 || cartTotalItems === 0) {
-    shipping = (0.0).toFixed(2);
-    freeShipping = "free shipping on orders over $500!";
-  } else {
-    shipping = (15.0).toFixed(2);
-  }
-
-  const cartTotalPrice = (
-    parseFloat(cartSubtotalPrice) + parseFloat(shipping)
-  ).toFixed(2);
-
   return (
     <div className="cart-page-summary-container">
-      <h1>Order Summary</h1>
-      <div className="cart-page-summary-info">
-        <span className="cart-page-summary-info-left">
-          <strong>Subtotal:</strong>
-          <p>
-            {cartTotalItems} {cartTotalItems > 1 ? "items" : "item"}
-          </p>
-        </span>
-        <span className="cart-page-summary-info-right">
-          ${cartSubtotalPrice}
-        </span>
-      </div>
-      <div className="cart-page-summary-info">
-        <span className="cart-page-summary-info-left">
-          <strong>Shipping:</strong>
-          <p className="free-shipping">{freeShipping}</p>
-        </span>
-        <span className="cart-page-summary-info-right">${shipping}</span>
-      </div>
-
-      <div className="cart-page-summary-info">
-        <span className="cart-page-summary-info-left">
-          <strong>Total:</strong>
-        </span>
-        <span className="cart-page-summary-info-right">
-          <strong>${cartTotalPrice}</strong>
-        </span>
-      </div>
+      <Link to="/shop">
+        <button className="go-back-btn">Go Back</button>
+      </Link>
+      <span className="total">Total ${cartTotalPrice}</span>
       <button className="checkout-btn">
         <span>CHECKOUT</span>
       </button>
