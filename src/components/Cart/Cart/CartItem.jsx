776d7a9eabc3862736e1c8ea583a779804e89a5e
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-11 15:15:00 +0100

    fixed a minor bug div cannot be desendant of a <p>

--- src/components/Cart/Cart/CartItem.jsx
+++ src/components/Cart/Cart/CartItem.jsx
@@ -19,7 +19,7 @@ function CartItem({
       <img src={Src} alt="Jordans"></img>
       <div className="cart-item-details">
         <h3 className="cart-item-name">{Name}</h3>{" "}
-        <p className="cart-item-quantity">
+        <div className="cart-item-quantity">
           {`Quantity: ${Quantity}`}
           <div className="quantity-btns-container">
             <button onClick={handleDecrease} disabled={Quantity < 2}>
@@ -27,7 +27,7 @@ function CartItem({
             </button>
             <button onClick={handleIncrease}>+</button>
           </div>
-        </p>
+        </div>
         <p className="cart-item-price">{`Price: ${Price.toFixed(
           2
         )} VAT included`}</p>
