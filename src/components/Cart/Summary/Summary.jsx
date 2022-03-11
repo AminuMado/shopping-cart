fb5bf753ae76c4377279e1cad8bc794be4a38a96
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-03-11 16:09:16 +0100

    added new modal and new functionality to the modal

--- src/components/Cart/Summary/Summary.jsx
+++ src/components/Cart/Summary/Summary.jsx
@@ -1,7 +1,9 @@
 import "./Summary.css";
 import { Link } from "react-router-dom";
+import React from "react";
 
-function Summary({ cartItems }) {
+function Summary({ cartItems, clearCartItems }) {
+  const [overlay, setOverLay] = React.useState(false);
   const cartTotalPrice = cartItems
     .map((item) => item.Price * item.Quantity)
     .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
@@ -13,17 +15,30 @@ function Summary({ cartItems }) {
         <button className="go-back-btn">Go Back</button>
       </Link>
       <span className="total">Total ${cartTotalPrice}</span>
-      <button className="checkout-btn">
+      <button className="checkout-btn" onClick={() => setOverLay(true)}>
         <span>CHECKOUT</span>
       </button>
-      <div class="checkout-message-container">
-        <h3>Retro Shop</h3>
-        <span>Thank you for your order!</span>
-        <p>This is actually a Fake Store</p>
-        <Link to="/Landing">
-          <button class="return-button">Return To Shop</button>
-        </Link>
-      </div>
+      {overlay && (
+        <>
+          <div className="checkout-message-container">
+            <h3>Retro Shop</h3>
+            <span>Thank you for your order!</span>
+            <p>This is actually a Fake Store</p>
+            <Link to="/">
+              <button
+                className="return-btn"
+                onClick={() => {
+                  setOverLay(false);
+                  clearCartItems();
+                }}
+              >
+                Return To Shop
+              </button>
+            </Link>
+          </div>
+          <div className="details-overlay active"></div>
+        </>
+      )}
     </div>
   );
 }
